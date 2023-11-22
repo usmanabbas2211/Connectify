111d82f82509cf326e27f46febbeafa5ea6de553
usmanabbas2211 <usmanabbas2211@gmail.com>
2023-11-22 18:18:19 +0500

    added ng-zorro

--- apps/connectify-fe/src/app/app.component.ts
+++ apps/connectify-fe/src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzMenuModule,
+  ],
   selector: 'connectify-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
+  styles: [
+    `
+      .logo {
+        width: 120px;
+        height: 31px;
+        background: rgba(255, 255, 255, 0.2);
+        margin: 16px 24px 16px 0;
+        float: left;
+      }
+
+      [nz-menu] {
+        line-height: 64px;
+      }
+
+      nz-breadcrumb {
+        margin: 16px 0;
+      }
+
+      nz-content {
+        padding: 0 50px;
+      }
+
+      nz-footer {
+        text-align: center;
+      }
+
+      .inner-content {
+        background: #fff;
+        padding: 24px;
+        min-height: 280px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'connectify-fe';
